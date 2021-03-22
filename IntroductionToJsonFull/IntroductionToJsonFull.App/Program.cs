7b10ca54cbd0807f9c453c1892ffdf2bc145595a
Chris Harrison <cah@stpaulsschool.org.uk>
2021-03-22 09:55:14 +0000

    .NET framework example

---
+++ IntroductionToJsonFull/IntroductionToJsonFull.App/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToJsonFull.App
+{
+    class Program
+    {
+        // step one - using the nuget package manager
+        // https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-in-visual-studio 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string json = @"{""key1"":""value1"",""key2"":""value2""}";
+
+            IDictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            foreach (string k in values.Keys)
+                Console.WriteLine($"{k}: {values[k]}");
+
+
+            // e.g. https://www.newtonsoft.com/json/help/html/DeserializeObject.htm 
+            string json2 = @"{
+            'Email': 'james@example.com',
+            'Active': true,
+            'CreatedDate': '2021-01-20T00:00:00Z',
+            'Roles': [
+                'User',
+                'Admin'
+            ]
+            }";
+
+            Account account = JsonConvert.DeserializeObject<Account>(json2);
+
+            Console.WriteLine(account.Email);
+
+        }
+
+        public class Account
+        {
+            string _email;
+            bool _active;
+            DateTime _created;
+            IList<string> _roles;
+
+            public string Email { get => _email; set => _email = value; }
+            public bool Active { get => _active; set => _active = value; }
+            public DateTime CreatedDate { get => _created; set => _created = value; }
+            public IList<string> Roles { get => _roles; set => _roles = value; }
+
+            public Account(string e, bool a, DateTime c, IList<string> r)
+            {
+                _email = e;
+                _active = a;
+                _created = c;
+                _roles = r;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Introduction to JSON / nuget packages
 
-JSON is a way of storing structured data, e.g. as lists, dictionaries or even potentially as objects, in text files. This is called "serialisation". 
+[JSON](https://www.w3schools.com/js/js_json_intro.asp) is a way of storing structured data, e.g. as lists, dictionaries or even potentially as objects, in text files. This is called "[serialisation](https://en.wikipedia.org/wiki/Serialization)". 
 
 The *de facto* way of working with JSON in C# is using the JSON.net library, which is not written by Microsoft, but which is available using the nuget package manager. 
+
+These examples can come from any text - a string, a local text file or, more exotically, a network connection to a remote API or (potentially RESTful) [webserver](https://github.com/jdorfman/Awesome-JSON-Datasets) (linked to, somewhat riskily, without fully checking the contents). 
 
 ## VS.Code
 
@@ -10,13 +12,16 @@ The C# project shown in the subdirectory `introduction-to-json` is VS.Code (load
 
 ### Using nuget 
 
-As outlined on (Stack Overflow)[https://stackoverflow.com/a/42218729/2902], open the terminal and type `dotnet add package Newtonsoft.Json`. 
+As outlined on [Stack Overflow](https://stackoverflow.com/a/42218729/2902), open the terminal and type `dotnet add package Newtonsoft.Json`. 
+
+Notice the new XML `ItemGroup` in the `csproj` file. 
 
 ## Visual Studio 
 
-The C# project .... 
+The C# solution shown in `IntroductionToJsonFull` is a Visual Studio .NET Framework solution.  
 
 ### Using nuget
 
-As outlined on (MSDN)[https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-in-visual-studio], right-click on the project > manage nuget > install `Newtonsoft.Json`. 
+As outlined on [MSDN](https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-in-visual-studio), right-click on the project > manage nuget > install `Newtonsoft.Json`. 
 
+Notice the new file `packages.config`.
